5be95b9ec39fe7ecc46acaad00260cadcd88a0ef
oman <smithowain@hotmail.com>
2025-01-08 11:54:03 +0900

    Blank webpage template

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     }),
   ],
   // ... other package.json stuff
-/*  "scripts": {
+  /*  "scripts": {
     "build": "webpack --config webpack.prod.js",
     "dev": "webpack serve --config webpack.dev.js"
     "deploy": "git subtree push --prefix dist origin gh-pages"
